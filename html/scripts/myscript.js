6c80fa5e5b771d1f98a31f23d6bc7d708502e46a
Asiantech.Php.Dung <loveisalie92@gmail.com>
2015-09-16 15:53:10 +0700

    master html

---
+++ html/scripts/myscript.js
@@ -0,0 +1,15 @@
+
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+
+
+	$(document).ready(function(){
+	
+	$('.home').addClass('current_page');
+	$('#navigationbar a').addClass('normal_page');
+	});
+
+	
+
+		
