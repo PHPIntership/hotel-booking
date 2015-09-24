346adde86a07c47e33ffd232f1cd9a3466f0c993
Pian DV <khdng0424@gmail.com>
2015-09-24 10:28:36 +0700

    change Hotel Model namspace

--- app/Hotel.php
+++ app/Hotel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace HotelBooking\Model;
+namespace HotelBooking;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +9,6 @@ class Hotel extends Model
 
     protected $table = 'hotels';
 
-    protected $fillable = ['name','quality','address','phone','website','image','description'];
+    protected $fillable = ['city_id','name','quality','address','phone','website','image','description'];
 
 }
