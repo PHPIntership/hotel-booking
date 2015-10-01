18e3ccb45361e2adad2f8d1286ef780568587788
Le Mai Vien <vienlm@asiantech.vn>
2015-10-01 09:47:02 +0700

    Migrate before test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,8 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
+        Artisan::call('migrate');
+
         return $app;
     }
 }
