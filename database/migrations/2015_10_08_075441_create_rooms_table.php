4c835d0450ef3d3fa7d5810d0e3a125688007923
Pian DV <khdng0424@gmail.com>
2015-10-08 15:11:05 +0700

    create Migration of Room and Room Model

---
+++ database/migrations/2015_10_08_075441_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hotel_room_type_id')->unsigned()->index();
+            $table->string('name');
+            $table->integer('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rooms');
+    }
+}
