bb866410aa2ca9ac7ef46e7d89f1e37c1d2180d5
minhduc1 <ductranminhitqb@gmail.com>
2015-10-08 15:14:46 +0700

    issue_32, create migration and model hotel room tyoe

---
+++ app/HotelRoomType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace HotelBooking;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class HotelRoomType extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'hotel_room_types';
+
+    /**
+     * Define attributes deleted_at of the data.
+     *
+     * @var string
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'roomtype_id',
+        'hotel_id',
+        'name',
+        'quality',
+        'quantity',
+        'price',
+        'image',
+        'description',
+    ];
+}
