b33edd2ef8ab7604ddfd9ecff8595bb11bb44cc1
Asiantech.Php.Dung <loveisalie92@gmail.com>
2015-10-08 15:51:49 +0700

    issue_42 Create hotel management template and layout

---
+++ resources/views/hotel/partials/navbar.blade.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-static-top" role="navigation">
+  <!-- Sidebar toggle button-->
+    <span class="sr-only">Toggle navigation</span>
+
+  <div class="navbar-custom-menu">
+    <ul class="nav navbar-nav">
+
+
+      <!-- User Account: style can be found in dropdown.less -->
+      <li class="dropdown user user-menu">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <img src="{{asset('bower_components/AdminLTE/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
+          <span class="hidden-xs">Alexander Pierce</span>
+        </a>
+        <ul class="dropdown-menu">
+          <!-- User image -->
+          <li class="user-header">
+            <img src="{{asset('bower_components/AdminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+            <p>
+              Alexander Pierce - Web Developer
+              <small>Member since Nov. 2012</small>
+            </p>
+          </li>
+          <!-- Menu Body -->
+          <li class="user-body">
+            <div class="col-xs-4 text-center">
+              <a href="#">Followers</a>
+            </div>
+            <div class="col-xs-4 text-center">
+              <a href="#">Sales</a>
+            </div>
+            <div class="col-xs-4 text-center">
+              <a href="#">Friends</a>
+            </div>
+          </li>
+          <!-- Menu Footer-->
+          <li class="user-footer">
+            <div class="pull-left">
+              <a href="#" class="btn btn-default btn-flat">Profile</a>
+            </div>
+            <div class="pull-right">
+              <a href="#" class="btn btn-default btn-flat">Sign out</a>
+            </div>
+          </li>
+        </ul>
+      </li>
+      <!-- Control Sidebar Toggle Button -->
+      <li>
+        <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
