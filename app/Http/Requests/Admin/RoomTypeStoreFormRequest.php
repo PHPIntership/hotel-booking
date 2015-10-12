12a6ad331947fb83bfd2c74d2b624bc14e3b29a0
VanThinh <vanthinh991@gmail.com>
2015-10-12 13:55:20 +0700

    Create and edit room type

---
+++ app/Http/Requests/Admin/RoomTypeStoreFormRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HotelBooking\Http\Requests\Admin;
+
+use HotelBooking\Http\Requests\Request;
+
+class RoomTypeStoreFormRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'quality' => 'required',
+            'image' => 'image',
+        ];
+    }
+}
---
+++ resources/views/admin/room-type/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.admin')
+@section('title')
+    {{trans('messages.room_type')}}
+@endsection
+@section('content')
+
+<div class="row " id="example2">
+<div class="col-md-12">
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">{!! trans('messages.room_type') !!}</h3>
+    </div>
+    <div class="box-body">
+        @include('layouts.admin.partials.flash')
+        <table class="table table-bordered table-striped" id="example1_wrapper">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>quality</th>
+                        <th>image</th>
+                        <th>action</th>
+                </thead>
+                <tbody>
+                @foreach ($roomTypes  as $key => $roomType)
+                <tr>
+                    <td>{{ $key+1 }}</td>
+                    <td>{{ $roomType->name }}</td>
+                    <td>{{ $roomType->quality }}</td>
+                    <td>{{ $roomType->image }}</td>
+                    <td>
+                        <a href="{!! route('admin.room-type.edit',$roomType->id) !!}" class="btn btn-info">
+                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            {!! trans('messages.edit') !!}
+                        </a>
+                        {!! Former::open()->route('admin.room-type.destroy',$roomType->id)->method('Delete')->style('display: inline') !!}
+                        <button onclick="return message('{!! trans('messages.delete_confirm') !!}')" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            {!! trans('messages.delete') !!}
+                        </button>
+                        {!! Former::close() !!}
+                    </td>
+                </tr>
+                @endforeach
+              </tbody>
+          </table>
+        </div>
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4" > {!! $roomTypes->render() !!} </div>
+    </div>
+</div>
+</div>
+@endsection
+@section('js')
+<script src="{{asset('assets/js/message.js')}}"></script>
+@endsection
