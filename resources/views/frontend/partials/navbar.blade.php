968f3ee538366078c90fb0af127a100cd9da62be
Asiantech.Php.Dung <loveisalie92@gmail.com>
2015-10-13 08:53:47 +0700

    fixes #59 - Create master page and partials for frontend page

---
+++ resources/views/frontend/partials/navbar.blade.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="row navall">
+        <nav class="navbar navbar-default" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="mynav_wrapper">
+                <div class="collapse navbar-collapse navbar-ex1-collapse mynav">
+                    <ul class="nav navbar-nav">
+                        <li class="active_item"><a href="#">Home</a></li>
+                        <li><a href="#">search</a></li>
+                        <li><a href="#">hotel</a></li>
+                        <li><a href="#">profile</a></li>
+                        <li><a href="#">gallery</a></li>
+                        <li><a href="#">contact</a></li>
+
+                    </ul>
+
+
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </nav>
+    </div>
+</div>
