5d47e354c42558784599b957e53636c8a313368c
Asiantech.Php.Dung <loveisalie92@gmail.com>
2015-10-14 14:30:37 +0700

    fixed #55 User login
    fix middleware convention error

--- app/Http/Middleware/Frontend/Authenticate.php
+++ app/Http/Middleware/Frontend/Authenticate.php
@@ -23,11 +23,13 @@ class Authenticate
     {
         $this->auth = Auth::user();
     }
+    
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
