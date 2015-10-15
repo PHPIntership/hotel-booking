ef8785af57607213740daae8863e019f88dcd3a5
minhduc1 <ductranminhitqb@gmail.com>
2015-10-15 09:28:31 +0700

    closed #83 create migration/model order and checkin

---
+++ database/migrations/2015_10_15_021321_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('hotel_room_type_id')->unsigned()->index();
+            $table->date('coming_date');
+            $table->date('leave_date');
+            $table->float('price');
+            $table->string('comment')->nullable();
+            $table->integer('quantity');
+            $table->integer('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
