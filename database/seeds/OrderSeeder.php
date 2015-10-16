d5403cc551190f4bcc84d4674b10578b878f8634
minhduc1 <ductranminhitqb@gmail.com>
2015-10-16 09:03:02 +0700

    #61 issue_61

---
+++ database/seeds/OrderSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use HotelBooking\Order;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $orders = array();
+        for ($i = 1; $i < 10; ++$i) {
+            Order::create([
+                'user_id' => rand(0, 8),
+                'hotel_room_type_id' => $i,
+                'coming_date' => $faker->date,
+                'leave_date' => $faker->date,
+                'price' => rand(1, 100),
+                'quantity' => rand(1, 5),
+                'status' => 0,
+            ]);
+        }
+    }
+}
---
+++ resources/views/hotel/booking-manage/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.hotel')
+@section('title')
+Booking Manage
+@endsection
+@section('content')
+
+<div class="row " id="example2">
+    <div class="col-md-12">
+        <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">{!! trans('messages.booking_manage') !!}</h3>
+        </div>
+        <div class="box-body">
+            @include('layouts.hotel.partials.flash')
+            <table class="table table-bordered table-striped" id="example1_wrapper">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>order id</th>
+                            <th>User</th>
+                            <th>Room type</th>
+                            <th>quantity</th>
+                            <th>coming date</th>
+                            <th>leave date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($orders  as $key => $order)
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td>{{ $order->id }}</td>
+                            <td>{{ $order->user->name }}</td>
+                            <td>{{ $order->hotelRoomType->name }}</td>
+                            <td>{{ $order->quantity }}</td>
+                            <td>{{ $order->coming_date }}</td>
+                            <td>{{ $order->leave_date }}</td>
+                            <td>
+                                <label style="font-size:13px" class="label label-{{$label[$order->status]}}">
+                                {{ trans('messages.status_order_'.$order->status) }}
+                                </label>
+                            </td>
+                            <td>
+                                @if ($order->status == 0)
+                                    {!! Former::open(route('hotel.booking-manage.accept',$order->id))->method('post')->style('display: inline') !!}
+                                        {!! Former::submit(trans('messages.accept'))->class("btn btn-sm btn-info")->onclick("return message('".trans('messages.accept_order_confirm')."')") !!}
+                                    {!! Former::close() !!}
+                                    {!! Former::open(route('hotel.booking-manage.decline',$order->id))->method('post')->style('display: inline') !!}
+                                        {!! Former::submit(trans('messages.decline'))->class("btn btn-sm btn-warning")->onclick("return message('".trans('messages.decline_order_confirm')."')") !!}
+                                    {!! Former::close() !!}
+                                @elseif ($order->status == 1)
+                                    {!! Former::open(route('hotel.booking-manage.cancel',$order->id))->method('post')->style('display: inline') !!}
+                                        {!! Former::submit(trans('messages.cancel'))->class("btn btn-sm btn-warning")->onclick("return message('".trans('messages.cancel_order_confirm')."')") !!}
+                                    {!! Former::close() !!}
+                                @elseif ($order->status == 3)
+                                    {!! Former::open(route('hotel.booking-manage.destroy',$order->id))->method('delete')->style('display: inline') !!}
+                                        {!! Former::submit(trans('messages.delete'))->class("btn btn-sm btn-danger")->onclick("return message('".trans('messages.delete_order_confirm')."')") !!}
+                                    {!! Former::close() !!}
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+            </table>
+        </div>
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4" >{!! $orders->render() !!}</div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('js')
+<script src="{{asset('assets/js/message.js')}}"></script>
+@endsection
